a39611cfa2135d4450f9a68d02d4dc4b527bc200
Biswajit <biswajitpatro888outlook.com>
2020-04-03 16:19:17 +0530

    Initial commit

---
+++ Assignment2/Controllers/EmployeesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assignment2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment2.Controllers
+{
+    public class EmployeesController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            ViewBag.data = "Employee Data In  view Bag";
+            ViewData["data"] = "Employee data in View Data";
+            List<Employee> empobj = new List<Employee>()
+        {
+            new Employee(){Id=1,name="Biswajit",sal=45000},
+            new Employee(){Id=2,name="Subrat",sal=50000},
+            new Employee(){Id=3,name="Yesh",sal=4000},
+            new Employee(){Id=4,name="Dev",sal=150000},
+            new Employee(){Id=5,name="Devid",sal=54500},
+
+        };
+            ViewBag.data1 = empobj;
+            return View(empobj);
+        }
+    }
+}
---
+++ Assignment2/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment2.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public float sal { get; set; }
+    }
+}
